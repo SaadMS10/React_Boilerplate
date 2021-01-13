5ddecd26e030272a6e12d9a5da9f224cc09af99b
Haider Raza <haider@Haiders-Mac-mini.local>
2021-01-13 13:57:52 +0500

    some updates

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Context = React.createContext({
-    _setState: (name, val) => { }
+    _setState: (name, val) => { },
+    state: {}
 });
 
 export default Context;
