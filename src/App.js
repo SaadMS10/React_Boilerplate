fe5b9946ab97cff5a41c208fb6ea9034bca9f2b1
Haider Raza <haider@Haiders-Mac-mini.local>
2021-01-13 14:43:25 +0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ContextWrapper from './context/ContextWrapper';
 import Routes from './routes'
+import 'node-sass';
+import "bootstrap";
 
 const App = () => {
   return (
